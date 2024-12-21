d75f82e23bc04c887d1431b7844d793cf9835dae
Alex Wachowski <Alex.Wachowski@tjc.tv>
2024-12-21 16:55:50 -0600

    skipping ssr for map component

--- app/components/MapComponent.js
+++ app/components/MapComponent.js
@@ -6,6 +6,9 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const MapComponent = () => {
   const position = [29.9915, -98.0915]; // Approximate latitude/longitude for "2V2X+6M"
+  if (typeof window === 'undefined') {
+    return null; // Skip rendering on the server
+  }
 
   return (
     <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>

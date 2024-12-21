b4c4018faaffcfaf8b9307190aedafe287a073f7
Alex Wachowski <Alex.Wachowski@tjc.tv>
2024-12-21 16:52:24 -0600

    made map component client rendering

--- app/components/MapComponent.js
+++ app/components/MapComponent.js
@@ -1,35 +1,5 @@
-// // components/MapComponent.js
+"use client"
 
-
-// import { useEffect } from 'react';
-// import L from 'leaflet';
-
-// const MapComponent = () => {
-//   useEffect(() => {
-//     // Initialize the map when the component is mounted on the client side
-//     const map = L.map('map').setView([30.035, -98.092], 13); // Coordinates for Wimberley, TX
-
-//     // Add OpenStreetMap tile layer
-//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-//     }).addTo(map);
-
-//     // Add a marker
-//     L.marker([30.035, -98.092])
-//       .addTo(map)
-//       .bindPopup('Jeremiah Hernandez - Training Location')
-//       .openPopup();
-
-//     // Cleanup function to remove the map instance when the component unmounts
-//     return () => {
-//       map.remove();
-//     };
-//   }, []);
-
-//   return <div id="map" className="w-full h-96 rounded-lg shadow-lg"></div>;
-// };
-
-// export default MapComponent;
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client"
 
 import Image from "next/image";
 import MapComponent from "./components/MapComponent.js";
